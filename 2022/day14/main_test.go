d3d47c1916099cc6a3d6c9f8f12c9559a5b769b9
LRueckert <lars.rueckert@data4life.care>
2022-12-14 20:25:38 +0100

    solve 2022 day 14

---
+++ 2022/day14/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetResult(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{
+		{"A", 24},
+		{"B", 93},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			file = fmt.Sprintf("test%s.txt", tt.name)
+			if got := getResult(tt.name); got != tt.want {
+				t.Errorf("GetResult() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
